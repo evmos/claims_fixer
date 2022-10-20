3d404aa79536d527b9f291050f2a587201d1143f
hanchon <guillermo.paoletti@gmail.com>
2022-10-20 14:20:53 +0200

    feat: create the accounts file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 
 	"github.com/hanchon/claims-fixer/internal"
@@ -107,6 +108,48 @@ func main() {
 		wg.Wait()
 		process.TxCommit()
 		fmt.Println("Everything finished correctly")
+	} else if os.Args[1] == "create" {
+		fmt.Println("Creating go file for the upgrade...")
+
+		dbToRead := internal.OpenDatabase("./results.db")
+		fmt.Println("Database opened")
+
+		// Get all the rows without the ibc account that was fixed via governance
+		rows, err := dbToRead.Query("select address, amount from claims where address != \"evmos1a53udazy8ayufvy0s434pfwjcedzqv345dnt3x\" order by address")
+		if err != nil {
+			fmt.Println("Error reading addresses", err)
+			return
+		}
+
+		accountsString := ""
+		totalRows := 0
+
+		for rows.Next() {
+			var address string
+			var amount string
+			err := rows.Scan(&address, &amount)
+			if err != nil {
+				fmt.Println("Error getting row!", err)
+				return
+			}
+			accountsString = accountsString + "\t{\"" + address + "\", \"" + amount + "\"},\n"
+			totalRows++
+		}
+
+		fmt.Println("Finished getting all the addresses")
+		rows.Close()
+		dbToRead.Close()
+
+		header := "package v9\n\nvar Accounts = [" + strconv.FormatInt(int64(totalRows), 10) + "][2]string{\n"
+		data := header + accountsString + "}\n"
+		// NOTE: we are not creating a .go file, because it will break this project build
+		err = os.WriteFile("accounts.txt", []byte(data), 0644)
+		if err != nil {
+			fmt.Println("Error on write file:", err)
+			panic("Stop execution")
+		}
+		fmt.Println("File saved as accounts.txt, rename it to .go when moving it to the evmos repo!")
+
 	} else {
 		fmt.Println("Invalid option")
 	}
